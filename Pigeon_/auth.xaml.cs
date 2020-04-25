743c37e9153326042e5eb03758f95da407720d00
echpoch2 <61111925+echpoch2@users.noreply.github.com>
2020-04-25 22:22:30 +0300

    Add files via upload

---
+++ Pigeon_/auth.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pigeon_
+{
+    /// <summary>
+    /// Логика взаимодействия для auth.xaml
+    /// </summary>
+    public partial class auth : Page
+    {
+        public auth()
+        {
+           InitializeComponent();
+           DataContext = ViewModel.getInstance();
+
+        }
+    }
+}
